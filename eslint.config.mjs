8ea6b2c64caf36c5df5d922d9db74188c803c88f
Robson Formigão <rformigao.gomes@gmail.com>
2025-01-18 13:20:59 -0300

    init boilerplate

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,30 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+/** @type {import('eslint').Linter.FlatConfig} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: globals.browser
+    },
+    plugins: {
+      react: pluginReact,
+      '@eslint/js': pluginJs,
+      'typescript-eslint': tseslint,
+      'react-hooks': eslintPluginReactHooks
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended
+]
